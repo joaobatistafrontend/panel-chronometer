46fa0d8f21f40a67b1713ae8447ff460874f9fc1
joaobatistafrontend <jbbuno007@gmail.com>
2022-10-04 17:30:31 -0300

    finalidazo cronometro em js

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 var seg = 60
 var min = 3
 var interval
+var pausado = document.getElementById('pausado')
+function doisDig(digito){
+    if(digito<10){
+        return('0'+digito)
+    }else{
+        return(digito)
+    }
+}
 
 
 function inicia(){
   interval = setInterval(relogio,10)
+  if (pausado.style.display ==='block'){
+    pausado.style.display ='none'
+    paudado.style.cor="red"
+
+  }else{
+        pausado.style.display ==='block'
+    }
 }
+// function divSucess(message){
+//     return `<p style="backgroud-color: green">${message}</p>`
+// }
+
 function pausa(){
     clearInterval(interval)
+    if (pausado.style.display ==='none'){
+        pausado.style.display ='block'
+    }else{
+        pausado.style.display ==='none'
+    }
+
+    // if (p)
+    // document.getElementById('message-success').innerHTML = divSucess('Tempo pausado com sucesso.')
+    
 }
 function parar(){
     clearInterval(interval)
     seg = 00
-    min = 03
-    document.getElementById('relogio').innerHTML = '03:00'
-
+    min = 3
+    document.getElementById('relogio').innerHTML = '00:00'
 }
 
 function relogio(){
@@ -36,5 +63,45 @@ function relogio(){
     
     console.log(min)
     console.log(seg)
-    document.getElementById('relogio').innerHTML = min + ':' + seg
+    cor (document.getElementById('relogio').innerHTML = doisDig(min)+ ':' + doisDig(seg))
+}
+// function um(){(min == min)=> min++}
+
+function um(){
+    if (min == min){
+        min ++
+    } 
+}
+function  dois(){
+    if (min == min){
+        min = min +2
+    } 
+}
+function  tres(){
+    if (min == min){
+        min = min +3
+    }    
+}
+function  quatro(){
+    if (min == min){
+        min = min +4
+    } 
+}
+function  cinco(){
+    if (min == min){
+        min = min +5
+    } 
+}
+
+function cor(){
+    if(min >=2){
+        document.getElementById('relogio').style.color = "green";
+    }
+    if (min <1){
+        document.getElementById('relogio').style.color = "red";
+    }
+}
+
+function pausado(){
+    hidden=false
 }
